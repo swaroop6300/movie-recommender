e769942a2e611110b4779d9112206483352b08d8
swaroop6300 <142043952+swaroop6300@users.noreply.github.com>
2025-09-19 16:16:33 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,19 @@ import streamlit as st
 import pickle
 import pandas as pd
 import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-# Load preprocessed data and similarity matrix
+# Load preprocessed movie data
 movies_dict = pickle.load(open('movies_dict.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
-similarity = pickle.load(open('similarity.pkl', 'rb'))   # <-- you need to save this in your ML project
 
+# Compute similarity at runtime
+cv = CountVectorizer(max_features=5000, stop_words='english')
+vectors = cv.fit_transform(movies['tags']).toarray()
+similarity = cosine_similarity(vectors)
+
+# Streamlit UI
 st.title("Movie Recommendation System 🎬")
 
 # Dropdown for movie selection
@@ -16,28 +23,13 @@ selected_movie_name = st.selectbox(
     movies['title'].values
 )
 
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-
-# Create tags column DataFrame from movies_dict
-movies = pd.DataFrame(movies_dict)
-
-cv = CountVectorizer(max_features=5000, stop_words='english')
-vectors = cv.fit_transform(movies['tags']).toarray()
-similarity = cosine_similarity(vectors)
-
-
-
-
 # Recommend function
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
     movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
 
-    recommended_movies = []
-    for i in movies_list:
-        recommended_movies.append(movies.iloc[i[0]].title)
+    recommended_movies = [movies.iloc[i[0]].title for i in movies_list]
     return recommended_movies
 
 # Button action
@@ -46,4 +38,3 @@ if st.button("Recommend"):
     st.write("**Recommended Movies:**")
     for movie in recommendations:
         st.write(movie)
-
